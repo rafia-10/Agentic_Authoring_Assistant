34ccf3f8abfdd389aab9d088c115d97a637ff272
rafia-10 <rafiaa05k@gmail.com>
2025-10-08 18:23:59 +0300

    remove unnecessary modules

--- input_handler.py
+++
@@ -1,9 +0,0 @@
-def collect_user_input():
-    user_input = {}
-
-    user_input['description'] = input("Enter your project description: ")
-    user_input['repo'] = input("Enter repository link (optional): ")
-    user_input['dataset'] = input("Enter dataset info (optional): ")
-    user_input['results'] = input("Enter experiment results (optional): ")
-
-    print("User input collected:", user_input)
