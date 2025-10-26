6255f1941136c080c2ad7ec3189091b7ee48ecfb
rafia-10 <rafiaa05k@gmail.com>
2025-10-26 21:35:29 +0300

    summary

--- agents/metadata_agent/summary_agent.py
+++ agents/metadata_agent/summary_agent.py
@@ -1,11 +1,12 @@
 # agents/summary_agent.py
 from tools.nlp_tool import NLPTool
 
+
 class SummaryAgent:
     def __init__(self, nlp_tool: NLPTool):
         self.nlp_tool = nlp_tool
         
 
     def generate_summary(self, description: str) -> str:
-        summary = self.nlp_tool.generate_summary(description)
+        summary = self.nlp_tool.generate_summary(description) 
         return summary
